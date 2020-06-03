7c8c6b7ab3fe295a2cb463810629bc988cc87cfd
Maribelle <maribelleleongzz@gmail.com>
2020-06-03 17:53:28 -0300

    added checking if file exists

--- fetcher.js
+++ fetcher.js
@@ -3,15 +3,36 @@ const fs = require('fs');
 const url = process.argv.slice(2, 3).join('');
 const destination = process.argv.slice(3, 4).join('');
 
-console.log(url);
-console.log(destination);
 request(url, (error, response, body) => {
-  //   console.log('body:', body); // Print the HTML for the Google homepage.
-  fs.writeFile(destination, body, (err) => {
+  fs.open(destination, 'wx', (err, fd) => {
     if (err) {
-      console.eror(err);
+      if (err.code === 'EEXIST') {
+        console.error(`${destination} already exists`);
+
+        return;
+      }
+
+      throw err;
+    }
+
+    writeMyData(body, destination);
+  });
+  //   fs.access(destination, fs.constants.F_OK, (err) => {
+  //     if (err) {
+  //       console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
+  //     } else {
+
+  //     }
+  //   });
+  //   console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+const writeMyData = (data, destination) => {
+  fs.writeFile(destination, data, (error) => {
+    if (error) {
+      console.eror(error);
     } else {
       console.log('wrote to file uccesfully');
     }
   });
-});
+};
