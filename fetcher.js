a8d66cbca91e6d306105447da17072cd70393a2d
Maribelle <maribelleleongzz@gmail.com>
2020-06-08 15:53:53 -0300

    prompted user whether to overwrite or not

--- fetcher.js
+++ fetcher.js
@@ -2,12 +2,30 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv.slice(2, 3).join('');
 const destination = process.argv.slice(3, 4).join('');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 request(url, (error, response, body) => {
   fs.open(destination, 'wx', (err, fd) => {
     if (err) {
       if (err.code === 'EEXIST') {
         console.error(`${destination} already exists`);
+
+        rl.question(
+          'Do you want to overwrite the file? If yes, please enter "Y". ',
+          (answer) => {
+            if (answer === 'Y') {
+              writeMyData(body, destination);
+            } else {
+              console.log('File remained unchanged.');
+            }
+            rl.close();
+          }
+        );
 
         return;
       }
@@ -17,14 +35,6 @@ request(url, (error, response, body) => {
 
     writeMyData(body, destination);
   });
-  //   fs.access(destination, fs.constants.F_OK, (err) => {
-  //     if (err) {
-  //       console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
-  //     } else {
-
-  //     }
-  //   });
-  //   console.log('body:', body); // Print the HTML for the Google homepage.
 });
 
 const writeMyData = (data, destination) => {
